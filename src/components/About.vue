7d7c087ff0e805f7d8129f1f7648e6d599e16d6b
filsammy <filsammy@gmail.com>
2025-09-12 12:31:56 +0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+   <!-- Start of About Me -->
+    <div class="row justify-content-center align-items-center py-4 py-lg-5" id="landing">
+        <div class="col-10 col-md-3 p-3 py-lg-5">
+            <img src="../assets/images/me1.png" class="rounded-circle border border-white img-fluid">
+        </div>
+        <div class="col-md-7 col-lg-5 text-white pl-md-5 py-lg-5">
+            <h1 class="mt-2 text-center text-md-left" id="myName">Fil Sammy</h1>
+            <h3 class="my-3 text-center text-md-left" id="myTitle">Full Stack Web Developer</h3>
+            <p class="text-justify landing-text">
+                I'm a career shifter who discovered the joy and excitement of web development and programming.
+            </p>
+        </div>
+    </div>
+    <!-- End of About Me -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="mt-5 p-3 text-light">
+      <h6 class="text-center">Fil Sammy</h6>
+      <p class="text-center mb-1">Full Stack Web Developer</p>
+      <p class="text-center">&copy; All Rights Reserved 2025</p>
+  </footer>
+</template>
+
+<style>
+</style>
